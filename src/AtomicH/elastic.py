971692b74ec7186ca37b1546f003244c36d80bb8
Melliott666 <morgan.elliott613@gmail.com>
2025-08-06 19:44:36 -0500

    Update elastic.py

--- src/AtomicH/elastic.py
+++ src/AtomicH/elastic.py
@@ -23,8 +23,8 @@ def Wave_Function(rhos,pin,l, mu, potential=potentials.Silvera_Triplet, int_type
 
     State = scipy.integrate.solve_ivp(ddxToint, (rhos[0], rhos[-1]), init, t_eval=rhos, method=int_type)
     Normalization = np.sqrt(State.y[0] ** 2 + (State.y[1] / pin) ** 2)
-    #return (np.sqrt(2 / np.pi) * State.y[0] / Normalization[-1], np.sqrt(2 / np.pi) * State.y[1] / Normalization[-1])
-    return (State.y[0] / Normalization[-1], State.y[1] / Normalization[-1])
+    return (np.sqrt(2 / np.pi) * State.y[0] / Normalization[-1], np.sqrt(2 / np.pi) * State.y[1] / Normalization[-1])
+    #return (State.y[0] / Normalization[-1], State.y[1] / Normalization[-1])
 
 def GetPhaseShift(rhos, p, l, mu, potential=potentials.Silvera_Triplet, how_to_int='Radau'):
 
@@ -32,18 +32,19 @@ def GetPhaseShift(rhos, p, l, mu, potential=potentials.Silvera_Triplet, how_to_i
     State = wf[0]
     dState_dx = wf[1]
 
-    #Big_delta_l = (rhos * dState_dx - State) / (rhos * State)
-    #jl_ka = spherical_jn(l, p * rhos)
-    #jl_prime_ka = spherical_jn(l, p * rhos, derivative=True)
-    #nl_ka = spherical_yn(l, p * rhos)
-    #nl_prime_ka = spherical_yn(l, p * rhos, derivative=True)
+    Big_delta_l = (rhos * dState_dx - State) / (rhos * State)
+    jl_ka = spherical_jn(l, p * rhos)
+    jl_prime_ka = spherical_jn(l, p * rhos, derivative=True)
+    nl_ka = spherical_yn(l, p * rhos)
+    nl_prime_ka = spherical_yn(l, p * rhos, derivative=True)
     
-    # deltas = np.arctan((p*jl_prime_ka - Big_delta_l*jl_ka) / (p*nl_prime_ka - Big_delta_l * nl_ka))
-    deltas = np.arctan(p * State / dState_dx) - p * rhos
+    deltas = np.arctan((p*jl_prime_ka - Big_delta_l*jl_ka) / (p*nl_prime_ka - Big_delta_l * nl_ka))
+    #deltas = np.arctan(p * State / dState_dx) - p * rhos
     return deltas
 
 def GetScatteringLength(rhos, p, l, mu, potential=potentials.Silvera_Triplet, how_to_int='Radau'):
     return -np.tan(GetPhaseShift(rhos, p, l, mu, potential, how_to_int)) / p
 
 def GetCrossSection(rhos, p, l, mu, potential=potentials.Silvera_Triplet, how_to_int='Radau'):
-    return 8*np.pi*GetScatteringLength(rhos, p, l, mu, potential, how_to_int)**2
+    return (8 * np.pi / p**2) * (2*l + 1) * np.sin(GetPhaseShift(rhos, p, l, mu, potential, how_to_int)[-1])**2
+    #return 8*np.pi*GetScatteringLength(rhos, p, l, mu, potential, how_to_int)**2
