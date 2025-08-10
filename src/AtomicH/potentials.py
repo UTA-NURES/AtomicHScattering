8dc7084a841c86007f683dca8de35527e5fe21f5
benjpjones <ben.jones@uta.edu>
2025-08-09 22:48:04 -0500

    Update to potentials

--- src/AtomicH/potentials.py
+++ src/AtomicH/potentials.py
@@ -16,22 +16,20 @@ path=os.path.dirname(os.path.abspath(__file__))
 #=====================================
 
 # From European Scientific Journal. 2012 Oct 1;8(24).
+#  Based on Silvera modified by Fried and Etters,
 def Silvera_Triplet(Rho):
-    R = Rho * hcInEVAngstrom / 4.16
+    rmin=4.16
+    x = Rho * hcInEVAngstrom / rmin
     D = 1.28
-    F=(R>D)+(R<D)*np.exp(-(D/R-1)**2)
-    return 6.46 * K2eV * (4.889e4*np.exp(0.0968-8.6403*R-2.427*R**2)-(1.365/R**6+0.425/R**8+0.183/R**10)*F)
+    F=(x>D)+(x<D)*np.exp(-(D/x-1)**2)
+    return 6.46 * K2eV * (4.889e4*np.exp(0.0968-8.6403*x-2.427*x**2)-(1.365/x**6+0.425/x**8+0.183/x**10)*F)
 
 # From Reviews of Modern Physics, 52(2), p.393. and Progress in Low Temperature Physics. 1986 Jan 1;10:139-370.
 def Silvera_Triplet2(Rho):
     R = Rho * hcInEVAngstrom / BohrInAng
-    P = np.exp(0.09678-1.10173*R-0.03945*R**2)+np.exp(-(10.04/R-1)**2)*(-6.5/R**6-124/R**8-3285/R**10)
+    P = np.exp(0.09678-1.10173*R-0.03945*R**2)+np.exp(-(10.0378/R-1)**2)*(-6.5/R**6-124/R**8-3285/R**10)
     return P * HartreeInEV
 
-def Silvera_Triplet2NoR8(Rho):
-    R = Rho * hcInEVAngstrom / BohrInAng
-    P = np.exp(0.09678-1.10173*R-0.03945*R**2)+np.exp(-(10.04/R-1)**2)*(-6.5/R**6)
-    return P * HartreeInEV
 
 # From Reviews of Modern Physics, 52(2), p.393.
 def Silvera_J(Rho):
@@ -57,6 +55,9 @@ CorrInterp=interp1d(dat_Adiabatic.R[0:-1]*BohrInAng/hcInEVAngstrom,Corr,bounds_e
 
 def UnCorrectedSilvera_Triplet(R):
     return Silvera_Triplet(R)*(1-CorrInterp(R))
+
+def AdiabaticCorr_Triplet(R):
+    return (Silvera_Triplet(R)*(2/3*CorrInterp(R)))
 
 def TCorrectedSilvera_Triplet(R):
     return Silvera_Triplet(R)*(1-(2/3)*CorrInterp(R))
@@ -92,15 +93,42 @@ def Kolos_SingletCombo(rho):
     Decide3=(Rp>=split2_3)
     return (Decide1*(interp_KolosSinglet1(Rp) + 1)  + Decide2*(interp_KolosSinglet2(Rp) + 1) + Decide3*(interp_KolosSinglet2(split2_3) + 1)*(Rp/split2_3)**-6) * HartreeInEV
 
-dat_KolosTriplet = np.genfromtxt(path+"/InputData/Triplet_Kolos1974.csv", delimiter=',', skip_header=1)
-interp_KolosTriplet = interp1d(dat_KolosTriplet[:,0], dat_KolosTriplet[:,1], kind = 'cubic', bounds_error = False, fill_value = 'extrapolate')
+# From Journal of molecular spectroscopy, 54(2), pp.303-311
+dat_KolosTriplet1 = np.genfromtxt(path+"/InputData/Triplet_Kolos1965.csv", delimiter=',', skip_header=1)
+# Kolos 1974, Chemical Physics Letters. 1974 Feb 15;24(4):457-60.
+dat_KolosTriplet2 = np.genfromtxt(path+"/InputData/Triplet_Kolos1974.csv", delimiter=',', skip_header=1)
+interp_KolosTriplet1 = interp1d(dat_KolosTriplet1[:,0], dat_KolosTriplet1[:,1], kind = 'linear', bounds_error = False, fill_value = 'extrapolate')
+interp_KolosTriplet2 = interp1d(dat_KolosTriplet2[:,0], dat_KolosTriplet2[:,1], kind = 'linear', bounds_error = False, fill_value = 'extrapolate')
 
 # From Journal of molecular spectroscopy, 54(2), pp.303-311
-def Kolos_Triplet(rho):
+def Kolos_Triplet1(rho):
     Rp = rho / BohrInAng * hcInEVAngstrom 
-    maxx = np.max(interp_KolosTriplet.x)
+    maxx = np.max(interp_KolosTriplet1.x)
     Decide = (Rp < maxx)
-    return (Decide * (interp_KolosTriplet(Rp) + 1) + (1 - Decide) * (interp_KolosTriplet(maxx) + 1) * (Rp / maxx) ** -6) * HartreeInEV
+    return ((Decide * (interp_KolosTriplet1(Rp) + 1) + (1 - Decide) * (interp_KolosTriplet1(maxx) + 1) * (Rp / maxx) ** -6) )* HartreeInEV
+
+def Kolos_TripletCombo(rho):
+    Rp = rho / BohrInAng * hcInEVAngstrom
+    split1_2=6  # Make the split within the range where function has support (between last 2 bins)
+    split2_3=11.5 #  [As above]
+    Decide1=(Rp<split1_2)
+    Decide2=(Rp>=split1_2)*(Rp<split2_3)
+    Decide3=(Rp>=split2_3)
+    return (Decide1*(interp_KolosTriplet1(Rp) + 1)  + Decide2*(interp_KolosTriplet2(Rp) + 1) + Decide3*(interp_KolosTriplet2(split2_3) + 1)*(Rp/split2_3)**-6) * HartreeInEV
+
+
+#==========================
+# Wolniewicz potentials
+#==========================
+
+# From The Journal of chemical physics, 99(3), pp.1851-1868.
+dat_WolniewiczSinglet     = np.genfromtxt(path+"/InputData/Singlet_Wolniewicz1993.csv", delimiter=',', skip_header=1)
+interp_WolniewiczSinglet  = interp1d(dat_WolniewiczSinglet[:,0], dat_WolniewiczSinglet[:,1], kind = 'linear', bounds_error = False, fill_value = 'extrapolate')
+def Wolniewicz_Singlet(rho):
+    Rp = rho / BohrInAng * hcInEVAngstrom
+    maxx = np.max(interp_WolniewiczSinglet.x)
+    Decide = (Rp < maxx)
+    return ((Decide * (interp_WolniewiczSinglet(Rp) + 1) + (1 - Decide) * (interp_WolniewiczSinglet(maxx) + 1) * (Rp / maxx) ** -6) )* HartreeInEV
 
 
 #==========================
@@ -108,10 +136,10 @@ def Kolos_Triplet(rho):
 #==========================
 # All from Physical Review A. 2000 Mar 6;61(4):042705.
 
-dat_Jamieson = pd.read_excel(path+"/InputData/Potential_Jamieson.xlsx")
-
-interp_JamiesonSinglet = interp1d(dat_Jamieson.R, dat_Jamieson.Singlet, kind = 'cubic',bounds_error=False, fill_value='extrapolate')
-interp_JamiesonTriplet = interp1d(dat_Jamieson.R, dat_Jamieson.Triplet, kind = 'cubic',bounds_error=False,fill_value='extrapolate')
+dat_JamiesonTriplet = np.genfromtxt(path+"/InputData/Triplet_Jamieson2000.csv", delimiter=',', skip_header=1)
+dat_JamiesonSinglet = np.genfromtxt(path+"/InputData/Singlet_Jamieson2000.csv", delimiter=',', skip_header=1)
+interp_JamiesonSinglet = interp1d(dat_JamiesonSinglet[:,0], dat_JamiesonSinglet[:,1], kind = 'cubic',bounds_error=False, fill_value='extrapolate')
+interp_JamiesonTriplet = interp1d(dat_JamiesonTriplet[:,0], dat_JamiesonTriplet[:,1], kind = 'cubic',bounds_error=False,fill_value='extrapolate')
 
 def Jamieson_Singlet(rho):
     return (interp_JamiesonSinglet(rho/BohrInAng * hcInEVAngstrom) + 1) * HartreeInEV
@@ -138,9 +166,9 @@ def Jamieson_Kolos_Mixed(rho):
     Decide1=(Rp<minx)
     Decide2 = (Rp>=minx)*(Rp<maxx)
     Decide3=(Rp>=maxx)
-    V1 = (interp_func(Rp) + 1) * Decide1
-    V2 = (interp_func_sing(Rp) + 1) * Decide2
-    V3 = (interp_func_sing(split2_3) + 1) * (Rp / split2_3) ** -6 * Decide3
+    V1 = (interp_KolosSinglet1(Rp) + 1) * Decide1
+    V2 = (interp_JamiesonSinglet(Rp) + 1) * Decide2
+    V3 = (interp_JamiesonSinglet(maxx) + 1) * (Rp / maxx) ** -6 * Decide3
 
     return (V1 + V2 + V3) * HartreeInEV
 
@@ -159,13 +187,15 @@ def DipoleRadialPart(rho):
 # Compilation of useful potentials
 #===========================================
 
-Triplets = {"Silvera":Silvera_Triplet,
+Triplets = {"Kolos 65":Kolos_Triplet1,
+            "Kolos 74":Kolos_TripletCombo,
+            "Silvera":Silvera_Triplet,
             "Silvera2":Silvera_Triplet2,
-            "Uncorrected Silvera":UnCorrectedSilvera_Triplet,
-            "SilveraNoR8":Silvera_Triplet2NoR8,
-            "Kolos":Kolos_Triplet,
             "Jamieson":Jamieson_TripletCombo}
 
-Singlets = {"Kolos":Kolos_SingletCombo,
-            "Pure Jamieson":Jamieson_SingletCombo,
-            "Kolos Jamieson Mixed":Jamieson_Kolos_Mixed}
+Singlets = {"Kolos 65":Kolos_Singlet1,
+            "Kolos 74":Kolos_SingletCombo,
+            "Silvera":Silvera_Singlet,
+            "Wolniewicz":Wolniewicz_Singlet,
+            "Jamieson":Jamieson_Kolos_Mixed}
+
